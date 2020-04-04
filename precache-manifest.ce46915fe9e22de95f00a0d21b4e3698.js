e179e1954b93804647c15dbc29e342859f263642
Aleksandra Karabasevic <avax88@hotmail.com>
2020-04-04 16:40:42 +0200

    Updates

---
+++ precache-manifest.ce46915fe9e22de95f00a0d21b4e3698.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "614002ab7e04419f8d326805dfef871d",
+    "url": "/beerapp/index.html"
+  },
+  {
+    "revision": "0491fae178b07efb62fd",
+    "url": "/beerapp/static/css/main.60c957c2.chunk.css"
+  },
+  {
+    "revision": "4f089ee919e3e3481eee",
+    "url": "/beerapp/static/js/2.f3651ee7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/beerapp/static/js/2.f3651ee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0491fae178b07efb62fd",
+    "url": "/beerapp/static/js/main.4bc299ad.chunk.js"
+  },
+  {
+    "revision": "ea67064905c7b0b299f0",
+    "url": "/beerapp/static/js/runtime-main.a872c26c.js"
+  }
+]);
